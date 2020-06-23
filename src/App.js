9f3e88fb313c37af1042e5203c48d3caf3b1d036
otaviopetry <otaviopetry@gmail.com>
2020-06-23 01:35:44 -0300

    Challenge completed. Passed test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import {
   SafeAreaView,
@@ -10,46 +10,80 @@ import {
   TouchableOpacity,
 } from "react-native";
 
+import api from "./services/api";
+
 export default function App() {
+
+  // create the projects STATE
+  const [projects, setProjects] = useState([]);
+
+  // create effect to populate projects
+  useEffect( () => {
+    api.get('/repositories').then( (response) => {
+      setProjects(response.data);
+    })
+  }, [])
+
   async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+
+    // store the updated project index
+    const projectIndex = projects.findIndex(project => project.id === id);
+    
+    // create the post request as async function    
+    const response = await api.post(`/repositories/${id}/like`);
+
+    // create the new project with the new like
+    const project = response.data;
+
+    // remove the project from array and insert the updated one
+    projects.splice(projectIndex, 1, project);
+
+    // change the state with the updated array, important to use the spread operator
+    setProjects([...projects]);
   }
 
   return (
-    <>
+    <>    
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
+        <FlatList 
+          data={projects}
+          keyExtractor={project => project.id}
+          renderItem={ ({ item: project }) => (
+            <View style={styles.repositoryContainer}>
+              <Text style={styles.repository}>{project.title}</Text>
 
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
+              <View style={styles.techsContainer}>
+                { project.techs.map( tech => (                  
+                    <Text key={tech} style={styles.tech}>
+                      {tech}
+                    </Text>                  
+                ))}
+              </View>
+              
 
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
+              <View style={styles.likesContainer}>
+                <Text
+                  style={styles.likeText}
+                  // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
+                  testID={`repository-likes-${project.id}`}
+                >
+                  {project.likes} curtidas
+                </Text>
+              </View>
 
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
-        </View>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleLikeRepository(project.id)}
+                // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
+                testID={`like-button-${project.id}`}
+              >
+                <Text style={styles.buttonText}>Curtir</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        
+        />        
       </SafeAreaView>
     </>
   );
@@ -72,6 +106,8 @@ const styles = StyleSheet.create({
   },
   techsContainer: {
     flexDirection: "row",
+    overflow: "hidden",
+    flexWrap: 'wrap',
     marginTop: 10,
   },
   tech: {
@@ -81,6 +117,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#04d361",
     paddingHorizontal: 10,
     paddingVertical: 5,
+    marginBottom: 10,
     color: "#fff",
   },
   likesContainer: {
